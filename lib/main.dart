26180109425cd14ab29e1eafe4c1811164ea7178
Bodri-Mbg1 <mbgbodri@gmail.com>
2025-02-05 17:26:13 +0100

    last commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,27 @@
 import 'package:examen_app/welcome.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+
+  if (kIsWeb) {
+    Firebase.initializeApp(
+      options: const FirebaseOptions(
+          apiKey: "AIzaSyDFgI_z9W5NQ0q1tKW2ExFV89p6ZQbRgas",
+          authDomain: "todolist-e2d0b.firebaseapp.com",
+          projectId: "todolist-e2d0b",
+          storageBucket: "todolist-e2d0b.firebasestorage.app",
+          messagingSenderId: "9318324827",
+          appId: "1:9318324827:web:d3196ca9ffe4865f803d7e",
+          measurementId: "G-38Y5PSS9F1"),
+    );
+  } else {
+    await Firebase.initializeApp();
+  }
+
   runApp(const MyApp());
 }
 
---
+++ lib/widgets/firebase_config.dart
@@ -0,0 +1,9 @@
+class FirebaseConfig {
+  static const String apiKey = "AIzaSyDFgI_z9W5NQ0q1tKW2ExFV89p6ZQbRgas";
+  static const String authDomain = "todolist-e2d0b.firebaseapp.com";
+  static const String projectId = "todolist-e2d0b";
+  static const String storageBucket = "todolist-e2d0b.firebasestorage.app";
+  static const String messagingSenderId = "9318324827";
+  static const String appId = "1:9318324827:web:d3196ca9ffe4865f803d7e";
+  static const String measurementId = "G-38Y5PSS9F1";
+}
